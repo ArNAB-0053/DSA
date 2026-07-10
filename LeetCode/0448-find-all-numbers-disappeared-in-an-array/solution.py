6d12e39c304011acd548e4cc08dce432b242fae1
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-07-11 03:33:23 +0530

    Kepr: Sync solution for "Find All Numbers Disappeared in an Array" [python3]

--- LeetCode/0448-find-all-numbers-disappeared-in-an-array/solution.py
+++ LeetCode/0448-find-all-numbers-disappeared-in-an-array/solution.py
@@ -18,18 +18,26 @@ class Solution:
 
         # return ans
 
+        # Also could've done - 
+        st = set(nums)
+        ans = []
+        for i in range(1, len(nums)+1):
+            if i not in st:
+                ans.append(i)
+        return ans
+
         # But we can do the marking in the same array but marking it negative
         # TC: O(n)
         # SC: O(1)
-        n = len(nums)
+        # n = len(nums)
 
-        for num in nums:
-            idx = abs(num)-1
-            nums[idx] = -abs(nums[idx])
+        # for num in nums:
+        #     idx = abs(num)-1
+        #     nums[idx] = -abs(nums[idx])
         
-        ans = []
-        for i, num in enumerate(nums):
-            if num > 0:
-                ans.append(i+1)
+        # ans = []
+        # for i, num in enumerate(nums):
+        #     if num > 0:
+        #         ans.append(i+1)
 
-        return ans
+        # return ans
