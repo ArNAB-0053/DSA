e64e9d16f3ab55f5d0e8a14644b01c9275ccee73
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-07-12 15:03:12 +0530

    Kepr: Sync solution for "Assign Cookies" [python3]

---
+++ LeetCode/0455-assign-cookies/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        gi = si = 0
+        while gi < len(g) and si < len(s):
+            if g[gi] <= s[si]:
+                gi += 1
+            si += 1
+        return gi
