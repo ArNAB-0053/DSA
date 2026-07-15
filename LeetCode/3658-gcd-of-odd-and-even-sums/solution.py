ffebe727738734d04566922a9f4abc572983a59d
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-07-15 22:18:34 +0530

    Kepr: Sync solution for "GCD of Odd and Even Sums" [python3]

---
+++ LeetCode/3658-gcd-of-odd-and-even-sums/solution.py
@@ -0,0 +1,15 @@
+# import math
+
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        # sumOdd = sumEven = 0
+
+        # for i in range(1, n*2):
+        #     if i % 2 == 0:
+        #         sumEven += i
+        #     else:
+        #         sumOdd += i
+
+        # return math.gcd(sumOdd, sumEven)
+
+        return n
