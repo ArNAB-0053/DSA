62ea83771507fc0b043a652b59800743c2c61efb
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-08-09 17:58:14 +0530

    Kepr: Sync solution for "Zig-Zag Tree Traversal" [python]

--- GeeksForGeeks/zigzag-tree-traversal/solution.py
+++ GeeksForGeeks/zigzag-tree-traversal/solution.py
@@ -8,9 +8,10 @@ class Node:
 from collections import deque
 class Solution:
     def zigZagTraversal(self, root):
+        if not root: return []
         q = deque([root])
         res = []
-        ltr = True
+        ltr = True # ltr -> eft to right
         while q:
             size = len(q)
             level = [0] * size
@@ -18,6 +19,10 @@ class Solution:
             for i in range(size):
                 node = q.popleft()
                 
+                # avoiding traversing later on with this optimization
+                # for ltr it will append from the left side
+                # whereas if not ltr then from right side
+                # it helps avoiding reversing the whole level array
                 if ltr:
                     level[i] = node.data
                 else:
@@ -30,6 +35,8 @@ class Solution:
                     q.append(node.right)
         
             ltr = not ltr
+            # as it doesn't want a array of array output but a single output
+            # we are only adding the values to res
             res += level
         
         return res
