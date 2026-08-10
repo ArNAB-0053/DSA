588d114f67a5344583fd6fb7b87e7e4753f2d25b
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-08-10 13:43:26 +0530

    Kepr: Sync solution for "Tree Boundary Traversal" [python]

--- GeeksForGeeks/boundary-traversal-of-binary-tree/solution.py
+++ GeeksForGeeks/boundary-traversal-of-binary-tree/solution.py
@@ -7,11 +7,13 @@ class Node:
         self.left = None
 '''
 class Solution:
+    # check is it a leaf node or not
     def isLeaf(self, node):
         if node.left or node.right:
             return False
         return True
         
+    # added left sided nodes of root to res except leaf nodes
     def addLeftNodes(self, root):
         curr = root.left
         
@@ -22,15 +24,20 @@ class Solution:
                 curr = curr.left
             else:
                 curr = curr.right
-        
+    
+    # adds only leaf nodes to res
     def addLeafNode(self, root):
         if self.isLeaf(root):
             self.res.append(root.data)
             return
         if root.left: self.addLeafNode(root.left)
         if root.right: self.addLeafNode(root.right)
-        
+    
+    # add right sided node of root to res except leaf nodes
     def addRightNodes(self, root):
+        # as it is acyclic boundary
+        # we have to reverse the right nodes
+        # so we are using temp and later add it's reversed value to res
         temp = []
         curr = root.right
         
@@ -40,18 +47,30 @@ class Solution:
                 
             if curr.right: curr = curr.right
             else: curr = curr.left
-            
+        
+        # adding temp's reverse value to res
         self.res += temp[::-1]
         
     def boundaryTraversal(self, root):
+        # if no root node
         if not root: return []
         
+        # if root node itself a leaf node
         if self.isLeaf(root): return [root.data]
         
+        # initializeing res with root.data
         self.res = [root.data]
-        self.addLeftNodes(root)
-        self.addLeafNode(root)
-        self.addRightNodes(root)
+        
+        # calling all the helper fns
+        
+        # as it want acyclic boundary traversal
+        # we do - 
+        # 1. all left nodes
+        # 2. all leaf nodes
+        # 3. all right nodes(reverse order)
+        self.addLeftNodes(root)     #1
+        self.addLeafNode(root)      #2
+        self.addRightNodes(root)    #3
         
         return self.res
         
