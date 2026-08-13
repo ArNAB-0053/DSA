7e9a7e3512762195dbe43704ca3b088363c04970
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-08-13 21:03:19 +0530

    Kepr: Sync solution for "Nodes at Distance in Binary Tree" [python]

---
+++ GeeksForGeeks/nodes-at-given-distance-in-binary-tree/solution.py
@@ -0,0 +1,56 @@
+'''
+Structure of Binary Tree Node
+ class Node:
+     def __init__(self, val):
+         self.data = val
+        self.left = None
+        self.right = None
+'''
+
+from collections import defaultdict, deque
+
+class Solution:
+    def kDistanceNodes(self, root, targetVal, k):
+        parents = {}
+        
+        target = None
+
+        # parent dict creating funtion   
+        def dfs(node, parent = None):
+            nonlocal target
+            if not node: return
+            if node.data == targetVal:
+                target = node
+            parents[node] = parent
+            if node.left: dfs(node.left, node)
+            if node.right: dfs(node.right, node)
+        
+        # fn call
+        dfs(root)
+        
+        # actual BFS (same as BFS on graph)
+        q = deque([target])
+        vis = {target}
+        d = 0 # distance
+        while q:
+            if d == k:
+                return sorted([node.data for node in q])
+
+            for _ in range(len(q)):
+                node = q.popleft()
+
+                neighbors = [
+                    node.left,
+                    node.right,
+                    parents[node]
+                ]
+
+                for neighbor in neighbors:
+                    if neighbor and neighbor not in vis:
+                        vis.add(neighbor)
+                        q.append(neighbor)
+
+            d += 1
+
+        return []
+        
