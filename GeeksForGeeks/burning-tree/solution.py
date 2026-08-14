57755ef55cf4538576292225b927ddd7f3e6e72f
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-08-14 14:20:56 +0530

    Kepr: Sync solution for "Burning Tree" [python]

--- GeeksForGeeks/burning-tree/solution.py
+++ GeeksForGeeks/burning-tree/solution.py
@@ -8,33 +8,46 @@ class Node:
 from collections import deque
 class Solution:
     def minTime(self, root, target):
-        parent = {}
+        # Intuition
         
-        target_node = None
+        # - store parent -> child in a map
+        # - try to create a proper undirected graph from it
+        # - find out the height based on depth
         
+        parent = {} # stores parent -> child
+        
+        target_node = None # given target is value, but I need node
+        
+        # TREE DFS
         def dfs(node, par=None):
             nonlocal target_node
             if not node: return 
         
+            # gives the targeted node
             if node.data == target:
                 target_node = node
-        
+            
+            # stores parent -> child
             parent[node] = par
             
             if node.left: dfs(node.left, node)
             if node.right: dfs(node.right, node)
-            
+        
+        # fn call
         dfs(root)
         
+        # from starts GRAPH BFS traversal
         q = deque([target_node])
         visited = {target_node}
         
-        height = -1
+        height = -1 # as it counts the edge
         
         while q:
             for _ in range(len(q)):
                 node = q.popleft()
                     
+                # those are all the neighbors
+                # or rather the traversal possible only to these
                 neighbors = [
                     node.left,
                     node.right,
@@ -46,7 +59,7 @@ class Solution:
                         visited.add(neighbor)
                         q.append(neighbor)
                 
-            
+            # height updation
             height += 1
             
         return height
