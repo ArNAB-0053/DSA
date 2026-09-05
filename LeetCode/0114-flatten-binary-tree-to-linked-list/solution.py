53df15d755ab1a2f9012b394530a72da3103784e
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-09-05 13:34:54 +0530

    Kepr: Sync solution for "Flatten Binary Tree to Linked List" [python3]

---
+++ LeetCode/0114-flatten-binary-tree-to-linked-list/solution.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        # ==============================================
+        # Recursion Approach - O(n) Time | O(n) Space
+        # ==============================================
+        # prev = None
+
+        # def do_flatten(node):
+        #     nonlocal prev
+
+        #     if node is None: return 
+
+        #     # go right
+        #     do_flatten(node.right)
+        #     # go left
+        #     do_flatten(node.left)
+
+        #     # connect node right to prev
+        #     node.right = prev
+        #     # disconnect left
+        #     node.left = None
+
+        #     # make prev as current node
+        #     prev = node
+
+        # do_flatten(root)
+
+        # ========================================
+        # Using Stack - O(n) Time | O(n) Space
+        # ========================================
+        # if not root: return
+
+        # stack = [root]
+
+        # while stack:
+        #     # get top-most element and pop it out
+        #     curr = stack.pop()
+        #     # append right
+        #     if curr.right: stack.append(curr.right)
+        #     # append left
+        #     if curr.left: stack.append(curr.left)
+
+        #     # the order of first right then left ensures left will be on top first when popping
+
+        #     # if stack not null
+        #     # connect curr right to top of stack
+        #     if stack: curr.right = stack[-1]
+        #     # and disconnect curr left
+        #     curr.left = None
+
+
+        # ========================================
+        # Morris Traversal - O(n) Time | O(1) Space
+        # ========================================
+        if not root: return
+
+        curr = root
+
+        while curr:
+            # when curr has left
+            if curr.left:
+                # prev is curr left
+                prev = curr.left
+
+                # move to the right most
+                while prev.right:
+                    prev = prev.right
+
+                # connect prev right with curr right
+                prev.right = curr.right
+                # curr right now will be curr left
+                curr.right = curr.left
+                # disconnect curr left
+                curr.left = None
+
+            # move curr
+            curr = curr.right
