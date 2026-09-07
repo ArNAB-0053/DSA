f54ac1cb173b9da9a1b63e3feabf725ff3253ab9
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-09-07 12:48:01 +0530

    Kepr: Sync solution for "Check for BST" [python]

--- GeeksForGeeks/check-for-bst/solution.py
+++ GeeksForGeeks/check-for-bst/solution.py
@@ -9,27 +9,42 @@ class Node:
 
 class Solution:
     def isBST(self, root: 'Node') -> bool:
-        prev = -1
-        
-        def inorder(root):
-            nonlocal prev
-            
+        # MIN-MAX APPROACH
+        def isValid(root, minn, maxx):
             if not root: return True
             
-            left = inorder(root.left)
-            if not left:
+            if not (minn < root.data < maxx):
                 return False
             
-            if prev >= root.data:
-                return False
+            left = isValid(root.left, minn, root.data)
+            right = isValid(root.right, root.data, maxx)
+        
+            return left and right
+            
+        return isValid(root, float('-inf'), float('inf'))
+        
+        # INORDER APPROACH
+        # prev = -1
+        
+        # def inorder(root):
+        #     nonlocal prev
+            
+        #     if not root: return True
+            
+        #     left = inorder(root.left)
+        #     if not left:
+        #         return False
+            
+        #     if prev >= root.data:
+        #         return False
                 
-            prev = root.data
+        #     prev = root.data
             
-            right = inorder(root.right)
-            if not right:
-                return False
+        #     right = inorder(root.right)
+        #     if not right:
+        #         return False
                 
-            return True
+        #     return True
             
-        return inorder(root)
+        # return inorder(root)
         
