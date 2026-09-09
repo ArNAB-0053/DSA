5d10a05f35036c7ded6edaa99bb44456b6b4b2aa
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-09-09 12:16:08 +0530

    Kepr: Sync solution for "Count Commas in Range II" [python3]

---
+++ LeetCode/3871-count-commas-in-range-ii/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        # ## Normal approach
+        # l = len(str(n))
+
+        # if l < 4:
+        #     return 0
+        
+        # if l == 4:
+        #     return n - 1000 + 1
+        
+        # if l > 4:
+        #     return int("9" * (l-4) + "000") * (l // 3 if l % 3 != 0 else l // 3 - 1) + (n - 10 ** (l-1)) + 1
+
+        ## Optimal approach
+        p = 1000
+        ans = 0
+        while p <= n:
+            ans += n - p + 1
+            p *= 1000
+
+        return ans
